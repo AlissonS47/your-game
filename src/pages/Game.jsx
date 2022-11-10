63e2db44913e9a84242bd3dee8d2367a7c89beb8
Alisson <tec.alissons47@gmail.com>
2022-11-10 10:34:04 -0300

    customização da scrollbar

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -16,6 +16,10 @@ const Game = () => {
   const { data: gameScreenshots } = useFetchGame(pk, 'screenshots');
   const { data: gameSeries } = useFetchGame(pk, 'game-series');
 
+  const namesList = (list) => {
+    return list.join(',');
+  };
+
   return (
     <>
       <Header bgImage={gameDetail && gameDetail.background_image} />
@@ -24,7 +28,7 @@ const Game = () => {
         <div id="game-page">
           <Container>
             <div className="game-page__content">
-              <div className="game-page__info flex-col-row flex-g2">
+              <section className="game-page__info flex-col-row flex-g2">
                 <div className="game-page__info-about">
                   <h2>{gameDetail.name}</h2>
                   <div dangerouslySetInnerHTML={{ __html: gameDetail.description }}></div>
@@ -34,21 +38,31 @@ const Game = () => {
                     <h3>Platforms</h3>
                     <p>
                       {gameDetail.platforms ?
-                        gameDetail.platforms.map((platformU) => `${platformU.platform.name}, `)
+                        gameDetail.platforms.map((platformU) => platformU.platform.name).join(', ')
                         : "Not informed"}
                     </p>
                   </div>
                   <div>
-                    <h3>Metascore</h3>
-                    {gameDetail.metacritic ?
-                      <MetaCritic gameScore={gameDetail.metacritic} />
-                      : "Not informed"}
+                    <h3>Developer</h3>
+                    <p>
+                      {gameDetail.developers ?
+                        gameDetail.developers.map((developer) => developer.name).join(', ')
+                        : "Not informed"}
+                    </p>
+                  </div>
+                  <div>
+                    <h3>Publisher</h3>
+                    <p>
+                      {gameDetail.publishers ?
+                        gameDetail.publishers.map((publisher) => publisher.name).join(', ')
+                        : "Not informed"}
+                    </p>
                   </div>
                   <div>
                     <h3>Genre</h3>
                     <p>
                       {gameDetail.genres ?
-                        gameDetail.genres.map((genre) => `${genre.name}, `)
+                        gameDetail.genres.map((genre) => genre.name).join(', ')
                         : "Not informed"}
                     </p>
                   </div>
@@ -61,20 +75,10 @@ const Game = () => {
                     </p>
                   </div>
                   <div>
-                    <h3>Developer</h3>
-                    <p>
-                      {gameDetail.developers ?
-                        gameDetail.developers.map((developer) => `${developer.name}, `)
-                        : "Not informed"}
-                    </p>
-                  </div>
-                  <div>
-                    <h3>Publisher</h3>
-                    <p>
-                      {gameDetail.publishers ?
-                        gameDetail.publishers.map((publisher) => publisher.name)
-                        : "Not informed"}
-                    </p>
+                    <h3>Metascore</h3>
+                    {gameDetail.metacritic ?
+                      <MetaCritic gameScore={gameDetail.metacritic} />
+                      : "Not informed"}
                   </div>
                   <div>
                     <h3>Playtime</h3>
@@ -93,24 +97,30 @@ const Game = () => {
                     </p>
                   </div>
                 </div>
-              </div>
-              <section id="game-page__screenshots">
-                <h2 className="text-center mt-3 mb-2">Screenshots</h2>
-                {!gameScreenshots && <PreLoader />}
-                {gameScreenshots && <Gallery images={gameScreenshots.results} />}
               </section>
+
+              <section className="game-page__media">
+                {gameScreenshots &&
+                  <>
+                    <h2 className="text-center mb-2">Screenshots</h2>
+                    <Gallery images={gameScreenshots.results} />
+                  </>
+                }
+              </section>
+
               <section id="game-page__game-series">
-                <h2 className="text-center mt-3 mb-2">Game Series</h2>
-                {!gameSeries && <PreLoader />}
                 {gameSeries &&
-                  <div className="flex-row flex-wrap flex-jc flex-g1">
-                    {gameSeries.results.map((game) => <GameCard game={game} key={game.id} />)}
-                  </div>
+                  <>
+                    <h2 className="text-center mt-3 mb-2">Game Series</h2>
+                    <div className="flex-row flex-wrap flex-jc flex-g1">
+                      {gameSeries.results.map((game) => <GameCard game={game} key={game.id} />)}
+                    </div>
+                  </>
                 }
               </section>
             </div>
           </Container>
-        </div>
+        </div >
       }
     </>
   )
